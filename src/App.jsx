155b7d73540eec52efba49814b35b776ff461ecb
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-12 22:21:07 -0800

    router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +7,12 @@ function App() {
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+        </div>
         <div id="main-section">{/* routes here */}</div>
       </div>
     </>
